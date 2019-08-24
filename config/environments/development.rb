a6d4db6e640e08149bdff2b916c121f729c399cd
conkonig <connor_stansfield@live.com>
2019-08-25 09:48:36 +1200

    added guard livereload and automated tests

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
+  config.middleware.insert_after(ActionDispatch::Static, Rack::LiveReload)
   # Settings specified here will take precedence over those in config/application.rb.
-
+  LISTEN_GEM_DEBUGGING=1
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
